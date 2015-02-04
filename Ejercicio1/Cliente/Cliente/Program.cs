179632d4b4eefd43469166316d0e4030a953300d
marco <marcomartinezavila@gmail.com>
2015-02-04 17:17:00 +0100

    Version inicial. No hay ni librerias ni pollas, el codigo del cliente y del servidor a palo seco. Al menos funciona

---
+++ Ejercicio1/Cliente/Cliente/Program.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Cliente
+{
+    class Client
+    {
+        public void Run()
+        {
+            String cadena = "hola";
+            Console.WriteLine("Cadena enviada: " + cadena);
+            byte[] bytes_cadena;
+
+            TcpClient client = null;
+            NetworkStream netStream = null;
+            try
+            {
+                client = new TcpClient("localhost", 23456);
+
+                netStream = client.GetStream();
+
+                using (MemoryStream ms = new MemoryStream())
+                using (BinaryWriter writer = new BinaryWriter(ms))
+                {
+                    writer.Write(cadena);
+
+                    writer.Flush();
+                    bytes_cadena = ms.ToArray();
+                }
+
+                Console.WriteLine("Lognitud: " + bytes_cadena.Length);
+
+                netStream.Write(bytes_cadena, 0, bytes_cadena.Length);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Exception: {0}", e.Message);
+            }
+            finally
+            {
+                netStream.Close();
+                client.Close();
+            }
+        }
+    }
+
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const int N = 100;
+
+            // Se crean los hilos
+            Thread[] threads = new Thread[N];
+            for (int i = 0; i < N; i++)
+            {
+                Client client = new Client();
+                threads[i] = new Thread(new ThreadStart(client.Run));
+                threads[i].Start();
+            }
+            // Se espera a que los hilos terminen
+            for (int i = 0; i < N; i++)
+            {
+                threads[i].Join();
+            }
+        }
+    }
+}
---
+++ Ejercicio1/Servidor/Servidor/Program.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Servidor
+{
+    class Server
+    {
+        public void Run()
+        {
+            Console.WriteLine("A ver ");
+            String cadena_rec = "";
+            byte[] bytes_rec = new byte[1024];
+            TcpListener listener = null;
+
+            try
+            {
+                listener = new TcpListener(IPAddress.Any, 23456);
+                listener.Start();
+            }
+            catch (SocketException se)
+            {
+                Console.WriteLine("Exception: {0}", se.Message);
+                return;
+            }
+
+            // El servidor se ejecuta infinitamente
+            for (; ; )
+            {
+                TcpClient client = null;
+                NetworkStream netStream = null;
+
+                try
+                {
+                    Console.WriteLine("A ver 2");
+                    client = listener.AcceptTcpClient();
+                    Console.WriteLine("A ver 3");
+                    netStream = client.GetStream();
+
+                    netStream.Read(bytes_rec, 0, bytes_rec.Length);
+
+                    Console.WriteLine("Lognitud: " + bytes_rec.Length);
+
+                    using (MemoryStream ms = new MemoryStream(bytes_rec))
+                    using (BinaryReader reader = new BinaryReader(ms))
+                    {
+                        cadena_rec = reader.ReadString();
+                    }
+
+                    netStream.Close();
+                    client.Close();
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine("Exception: {0}", e.Message);
+                    netStream.Close();
+                }
+                Console.WriteLine("Cadena recibida: " + cadena_rec);
+            }
+        }
+    }
+
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Server serv = new Server();
+            Thread hilo = new Thread(new ThreadStart(serv.Run));
+            hilo.Start();
+            hilo.Join();
+        }
+    }
+}
+
