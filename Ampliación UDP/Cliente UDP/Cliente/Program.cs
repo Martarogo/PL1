5c9517c6bf1e535624dbb41dd10e8e8e413ebcd5
Marta Rodríguez <uo216988@uniovi.es>
2015-04-18 17:01:14 +0200

    Último commit
    No sé por qué pero no había incluido ese fichero en el último commit.
    Ahora sí que está todo.

--- Ampliación UDP/Cliente UDP/Cliente/Program.cs
+++ Ampliación UDP/Cliente UDP/Cliente/Program.cs
@@ -16,8 +16,6 @@ namespace Cliente
         //Variables:
         private static String SERVER = "localhost";
         private static int SERVERPORT = 23456;
-        private String cadena_recibo;
-        private byte[] bytes_recibo, bytes_envio = new byte[512];
         private BinaryEchoMessageCodec codec = new BinaryEchoMessageCodec();
                  
         //Run:
@@ -25,38 +23,35 @@ namespace Cliente
         {
             //Mensaje de inicio + introducir mensaje:
             Console.WriteLine("Escribe el texto que quieres enviar: ");
-            String cadena = Console.ReadLine();
-            EchoMessage mensaje = new EchoMessage(cadena);
-            bytes_envio = codec.Encode(mensaje);
-            send(bytes_envio);
-           
+            String str = Console.ReadLine();
+            EchoMessage msg = new EchoMessage(str);
+            byte[] bytesToSend = codec.Encode(msg);
+            send(bytesToSend);           
 
             //Para mantener la consola abierta hasta que se pulse una tecla:
             Console.ReadKey();
         }//Fin Run
 
-        private void send(byte[] cadena_envío)
+        private void send(byte[] strToSend)
         {
-            UdpClient clienteUDP = new UdpClient();
+            UdpClient udpClient = new UdpClient();
             try
             {
                 // Envíar información
-                clienteUDP.Send(cadena_envío, cadena_envío.Length, SERVER, SERVERPORT);
+                udpClient.Send(strToSend, strToSend.Length, SERVER, SERVERPORT);
 
                 IPEndPoint remoteIPEndPoint = new IPEndPoint(IPAddress.Any, 0);
 
                 // Recibir información
-                byte[] rcvPacket = clienteUDP.Receive(ref remoteIPEndPoint);
-                cadena_recibo = codec.Decode(rcvPacket);
+                byte[] rcvPacket = udpClient.Receive(ref remoteIPEndPoint);
+                Console.WriteLine("Cadena reenviada por el servidor: " + codec.Decode(rcvPacket));
+                udpClient.Close();
             }
             catch (Exception e)
             {
                 Console.WriteLine("Exception: "+e.Message);
                 return;
             }
-            Console.WriteLine("Cadena reenviada por el servidor: " + cadena_recibo);
-            clienteUDP.Close();
-
         }//Fin Send
 
         class Program
